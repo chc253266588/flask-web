c7ce45f4baf844dcfb767c2a62c2055f83d042c0
root <root@demo.(none)>
2016-06-03 18:37:02 +0800

    0603

--- hello.py
+++ hello.py
@@ -1,17 +1,40 @@
 from flask import Flask,render_template
-from flask.ext.script import Manager
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
+from flask_wtf import Form
+from wtforms import StringField,SubmitField,PasswordField
+from wtforms.validators import Required,Length
+
+class NameForm(Form):
+	name = StringField("username ",validators=[Required()])
+	passwd = PasswordField("password ",validators=[Length(6)]) 
+	submit = SubmitField('Submit')
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
-@app.route("/")
+app.config['SECRET_KEY'] = 'hard to guess string!'
+
+@app.route("/",methods=['GET','POST'])
 def index():
+	name = None
+	alert = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+		alert = 'please input you username!'
 	return render_template('index.html')
 
 @app.route("/user/<name>")
 def user(name):
 	return render_template("user.html",name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template("404.html"),404
 
 if __name__ == '__main__':
 	manager.run()
